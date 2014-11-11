454b437c03dd856cea9901fccfa07b79098cb97f
Virgil Dupras <hsoft@hardcoded.net>
2014-11-11 17:52:39 -0500

    Now with line parsing!

--- src/webtop.rs
+++ src/webtop.rs
@@ -1,10 +1,39 @@
 #![feature(globs)]
 
 extern crate ncurses;
+extern crate regex;
 
 use std::io::File;
 use std::io::fs::PathExtensions;
+use regex::Regex;
 use ncurses::*;
+
+struct Hit<'r> {
+    host: &'r str,
+    status: uint,
+    path: &'r str,
+    referer: &'r str,
+    agent: &'r str,
+}
+
+fn parse_line(line: &str) -> Option<Box<Hit>> {
+    let re = Regex::new(r#"(\d+\.\d+\.\d+\.\d+) - - \[.+\] "\w+ ([^ ]+) [^ "]+" (\d+) \d+ "([^"]*)" "([^"]*)""#)
+        .ok().expect("");
+    let cap = match re.captures(line) {
+        Some(cap) => cap,
+        None => return None
+    };
+    Some(box Hit {
+        host: cap.at(1),
+        status: match from_str(cap.at(3)) {
+            Some(i) => i,
+            None => 999
+        },
+        path: cap.at(2),
+        referer: cap.at(4),
+        agent: cap.at(5),
+    })
+}
 
 fn main()
 {
@@ -36,7 +65,15 @@ fn main()
         let contents = ::std::str::from_utf8(raw_contents.as_slice()).unwrap();
         let lines = contents.split('\n').rev();
         for (index, line) in lines.enumerate() {
-            mvprintw(index as i32, 0, line);
+            let hit_box = match parse_line(line) {
+                Some(hit_box) => hit_box,
+                None => continue
+            };
+            let hit_fmt = format!(
+                "{} | {} | {} | {} | {}",
+                hit_box.host, hit_box.status, hit_box.path, hit_box.referer, hit_box.agent
+            );
+            mvprintw(index as i32, 0, hit_fmt.as_slice());
             if index == maxlines-1 {
                 break;
             }

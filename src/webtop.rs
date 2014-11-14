148c99ebb3c9411830a323a5517141c5840feb38
Virgil Dupras <hsoft@hardcoded.net>
2014-11-14 17:04:01 -0500

    Group and count hits by host
    Boy was that a hard commit! Three days of head banging trying to grasp
    memory ownership.
    
    This barely compiles and I had to clone() everywhere. So, it's not
    efficient, but at least it compiles!
    
    Thanks to Gankro and reem on #rust for their help.

--- src/webtop.rs
+++ src/webtop.rs
@@ -6,28 +6,36 @@ extern crate time;
 
 use std::io::File;
 use std::io::fs::PathExtensions;
+use std::collections::hash_map::{HashMap, Occupied, Vacant};
 use time::{Tm, strptime, strftime, now};
 use regex::Regex;
 use ncurses::*;
 
-struct Hit<'r> {
-    host: &'r str,
+#[deriving(Clone)]
+struct Hit {
+    host: String,
     time: Tm,
     status: uint,
-    path: &'r str,
-    referer: &'r str,
-    agent: &'r str,
+    path: String,
+    referer: String,
+    agent: String,
 }
 
-fn parse_line(line: &str) -> Option<Box<Hit>> {
+struct HitCounter {
+    host: String,
+    last_hit: Box<Hit>,
+    count: uint,
+}
+
+fn parse_line(line: &str) -> Option<Hit> {
     let re = Regex::new(r#"(\d+\.\d+\.\d+\.\d+) - - \[(.+) \+\d{4}\] "\w+ ([^ ]+) [^ "]+" (\d+) \d+ "([^"]*)" "([^"]*)""#)
         .ok().expect("");
     let cap = match re.captures(line) {
         Some(cap) => cap,
         None => return None
     };
-    Some(box Hit {
-        host: cap.at(1),
+    Some(Hit {
+        host: cap.at(1).to_string(),
         time: match strptime(cap.at(2), "%d/%b/%Y:%H:%M:%S") {
             Ok(tm) => tm,
             Err(_) => now()
@@ -36,12 +44,62 @@ fn parse_line(line: &str) -> Option<Box<Hit>> {
             Some(i) => i,
             None => 999
         },
-        path: cap.at(3),
-        referer: cap.at(5),
-        agent: cap.at(6),
+        path: cap.at(3).to_string(),
+        referer: cap.at(5).to_string(),
+        agent: cap.at(6).to_string(),
     })
 }
 
+fn mainloop(filepath: &Path, maxlines: uint) {
+    let mut timer = ::std::io::Timer::new().unwrap();
+    let mut input : i32 = -1;
+    while input == -1 {
+        let mut fp = File::open(filepath).ok().expect("");
+        let _ = fp.seek(-20000, ::std::io::SeekEnd);
+        let raw_contents = fp.read_to_end().unwrap();
+        let contents = ::std::str::from_utf8(raw_contents.as_slice()).unwrap();
+        let mut counters: HashMap<String, HitCounter> = HashMap::new();
+        for line in contents.split('\n').rev() {
+            let hit_box = match parse_line(line) {
+                Some(hit) => box hit,
+                None => continue
+            };
+            let _ = match counters.entry(hit_box.clone().host.clone()) {
+                Vacant(_) => {}
+                Occupied(e) => {
+                    let mut counter: &mut HitCounter = e.into_mut();
+                    counter.count += 1;
+                    continue;
+                },
+            };
+            let counter = HitCounter {
+                host: hit_box.host.clone(),
+                last_hit: hit_box.clone(),
+                count: 1,
+            };
+            counters.insert(hit_box.host, counter);
+        }
+        let mut sorted_counters: Vec<&HitCounter> = counters.values().collect();
+        sorted_counters.sort_by(|a, b| (&b.count).cmp(&a.count));
+        for (index, counter) in sorted_counters.iter().take(maxlines).enumerate() {
+            let hit = counter.last_hit.clone();
+            let time_fmt = strftime("%Y-%m-%d %H:%M:%S", &hit.time).unwrap();
+            let hit_fmt = format!(
+                "{} | {} | {} | {} | {} | {}",
+                counter.count, hit.host, time_fmt, hit.status, hit.path, hit.referer
+            );
+            mvprintw(index as i32, 0, hit_fmt.as_slice());
+        }
+        let msg = format!(
+            "This program reads the last {} lines of {} and updates automatically",
+            maxlines, filepath.display()
+        );
+        mvprintw((maxlines+1) as i32, 0, msg.as_slice());
+        refresh();
+        timer.sleep(::std::time::Duration::milliseconds(1000));
+        input = getch();
+    }
+}
 fn main()
 {
     let args = ::std::os::args();
@@ -54,8 +112,6 @@ fn main()
         println!("{} doesn't exist! aborting.", filepath.display());
         return;
     }
-    let mut timer = ::std::io::Timer::new().unwrap();
-    let mut input : i32 = -1;
     initscr();
     raw();
     keypad(stdscr, true);
@@ -65,37 +121,8 @@ fn main()
     let scry = getmaxy(stdscr) as uint;
     let maxlines = scry - 2;
 
-    while input == -1 {
-        let mut fp = File::open(&filepath).ok().expect("");
-        let _ = fp.seek(-20000, ::std::io::SeekEnd);
-        let raw_contents = fp.read_to_end().unwrap();
-        let contents = ::std::str::from_utf8(raw_contents.as_slice()).unwrap();
-        let lines = contents.split('\n').rev();
-        for (index, line) in lines.enumerate() {
-            let hit_box = match parse_line(line) {
-                Some(hit_box) => hit_box,
-                None => continue
-            };
-            let time_fmt = strftime("%Y-%m-%d %H:%M:%S", &hit_box.time);
-            let hit_fmt = format!(
-                "{} | {} | {} | {} | {} | {}",
-                hit_box.host, time_fmt, hit_box.status, hit_box.path, hit_box.referer, hit_box.agent
-            );
-            mvprintw(index as i32, 0, hit_fmt.as_slice());
-            if index == maxlines-1 {
-                break;
-            }
-        }
-        let msg = format!(
-            "This program reads the last {} lines of {} and updates automatically",
-            maxlines, filepath.display()
-        );
-        mvprintw((maxlines+1) as i32, 0, msg.as_slice());
-        refresh();
-        timer.sleep(::std::time::Duration::milliseconds(1000));
-        input = getch();
-    }
+    mainloop(&filepath, maxlines);
+
     endwin();
-    println!("test {}", input);
 }
 

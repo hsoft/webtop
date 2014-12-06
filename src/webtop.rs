b488e5ff7aad5804dbb8bbdcd6c61ff2314c3c2d
Virgil Dupras <hsoft@hardcoded.net>
2014-12-06 11:46:30 -0500

    Add rudimentary Path mode
    I know that the `val0()` and `val1()` thing aren't elegant. I fought
    with the compiler to make unpacking work and I gave up (for now!).

--- src/webtop.rs
+++ src/webtop.rs
@@ -9,6 +9,7 @@ extern crate ncurses;
 use std::io::File;
 use std::io::fs::PathExtensions;
 use std::collections::hash_map::{HashMap, Occupied, Vacant};
+use std::collections::hash_set::HashSet;
 use time::{Tm, strftime, now};
 use ncurses::{
     mvprintw, refresh, erase, initscr, getch, raw, keypad, nodelay, noecho, stdscr, getmaxy, endwin
@@ -24,8 +25,10 @@ const HOST_KEY: i32 = 'h' as i32;
 const PATH_KEY: i32 = 'p' as i32;
 const REFERER_KEY: i32 = 'r' as i32;
 
-type VisitHolder = HashMap<uint, Box<Visit>>;
-type HostVisitMap = HashMap<String, uint>;
+type VisitID = uint;
+type VisitHolder = HashMap<VisitID, Box<Visit>>;
+type HostVisitMap = HashMap<String, VisitID>;
+type PathVisitMap = HashMap<String, Box<HashSet<VisitID>>>;
 
 enum ProgramMode {
     Host,
@@ -47,6 +50,44 @@ fn purge_visits(visits: &mut VisitHolder, host_visit_map: &mut HostVisitMap, las
     }
 }
 
+fn output_host_mode(visits: &VisitHolder, maxlines: uint) {
+    let mut sorted_visits: Vec<&Box<Visit>> = visits.values().collect();
+    sorted_visits.sort_by(
+        |a, b| match (&a.hit_count).cmp(&b.hit_count).reverse() {
+            Equal => a.last_hit_time.to_timespec().cmp(&b.last_hit_time.to_timespec()).reverse(),
+            x => x,
+        }
+    );
+    erase();
+    for (index, visit) in sorted_visits.iter().take(maxlines).enumerate() {
+        let time_fmt = strftime("%Y-%m-%d %H:%M:%S", &visit.last_hit_time).unwrap();
+        let visit_fmt = format!(
+            "{:>4} | {:<15} | {} | {} | {}",
+            visit.hit_count, visit.host, time_fmt, visit.last_path, visit.referer
+        );
+        mvprintw(index as i32, 0, visit_fmt.as_slice());
+    }
+}
+
+fn output_path_mode(path_visit_map: &PathVisitMap, maxlines: uint) {
+    let mut sorted_path_chunks: Vec<(&str, uint)> = path_visit_map.iter().map(
+        |(key, value)| (key.as_slice(), value.len())
+    ).collect();
+    sorted_path_chunks.sort_by(
+        |a, b| a.val1().cmp(&b.val1()).reverse()
+    );
+    erase();
+    for (index, pair) in sorted_path_chunks.iter().take(maxlines).enumerate() {
+        let path = pair.val0();
+        let visit_count = pair.val1();
+        let path_fmt = format!(
+            "{:>4} | {}",
+            visit_count, path,
+        );
+        mvprintw(index as i32, 0, path_fmt.as_slice());
+    }
+}
+
 fn mainloop(filepath: &Path, maxlines: uint) -> i32 {
     let mut timer = ::std::io::Timer::new().unwrap();
     let mut last_size: i64 = 0;
@@ -54,7 +95,8 @@ fn mainloop(filepath: &Path, maxlines: uint) -> i32 {
     let mut visits: VisitHolder = HashMap::new();
     let mut visit_counter:uint = 0;
     let mut host_visit_map: HostVisitMap = HashMap::new();
-    let mut mode = Host;
+    let mut path_visit_map: PathVisitMap = HashMap::new();
+    let mut mode = ProgramMode::Host;
     loop {
         let fsize = filepath.stat().ok().expect("can't stat").size as i64;
         if fsize < last_size {
@@ -84,7 +126,7 @@ fn mainloop(filepath: &Path, maxlines: uint) -> i32 {
                 None => continue
             };
             let key = &hit.host;
-            let visitid: uint = match host_visit_map.entry(key.clone()) {
+            let visitid: VisitID = match host_visit_map.entry(key.clone()) {
                 Occupied(e) => {
                     *e.get()
                 }
@@ -110,28 +152,28 @@ fn mainloop(filepath: &Path, maxlines: uint) -> i32 {
             visit.last_hit_time = hit.time;
             visit.last_path = hit.path.clone();
             last_seen_time = hit.time;
+            let key = &hit.path;
+            match path_visit_map.entry(key.clone()) {
+                Occupied(e) => {
+                    let visits: &mut Box<HashSet<uint>> = e.into_mut();
+                    visits.insert(visitid);
+                }
+                Vacant(e) => {
+                    let mut visits = box HashSet::new();
+                    visits.insert(visitid);
+                    e.set(visits);
+                }
+            };
         }
         purge_visits(&mut visits, &mut host_visit_map, last_seen_time);
-        let mut sorted_visits: Vec<&Box<Visit>> = visits.values().collect();
-        sorted_visits.sort_by(
-            |a, b| match (&a.hit_count).cmp(&b.hit_count).reverse() {
-                Equal => a.last_hit_time.to_timespec().cmp(&b.last_hit_time.to_timespec()).reverse(),
-                x => x,
-            }
-        );
-        erase();
-        for (index, visit) in sorted_visits.iter().take(maxlines).enumerate() {
-            let time_fmt = strftime("%Y-%m-%d %H:%M:%S", &visit.last_hit_time).unwrap();
-            let visit_fmt = format!(
-                "{:>4} | {:<15} | {} | {} | {}",
-                visit.hit_count, visit.host, time_fmt, visit.last_path, visit.referer
-            );
-            mvprintw(index as i32, 0, visit_fmt.as_slice());
-        }
+        match mode {
+            ProgramMode::URLPath => output_path_mode(&path_visit_map, maxlines),
+            _ => output_host_mode(&visits, maxlines),
+        };
         let mode_str = match mode {
-            Host => "Host",
-            URLPath => "Path",
-            Referer => "Referer",
+            ProgramMode::Host => "Host",
+            ProgramMode::URLPath => "Path",
+            ProgramMode::Referer => "Referer",
         };
         let msg = format!(
             "{} active visits. Last read: {} bytes. {} mode. Hit 'q' to quit, 'h/p/r' for the different modes",
@@ -143,9 +185,9 @@ fn mainloop(filepath: &Path, maxlines: uint) -> i32 {
         let input = getch();
         mode = match input {
             QUIT_KEY => return input,
-            PATH_KEY => URLPath,
-            HOST_KEY => Host,
-            REFERER_KEY => Referer,
+            PATH_KEY => ProgramMode::URLPath,
+            HOST_KEY => ProgramMode::Host,
+            REFERER_KEY => ProgramMode::Referer,
             _ => mode,
         }
     }
